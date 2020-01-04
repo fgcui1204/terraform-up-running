01d1ce54d23cec7b14d5c3227827d40d85a63e26
Fugang Cui <fugang.cui@zip.co>
2020-01-04 15:00:23 +0800

    init commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-03f562fd48c4c192f"
+  instance_type = "t3.small"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
